dde50e5a68dad781a3875a146fc7763d37a7ff35
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-06 17:23:05 +0300

    updated: get datas

--- app/src/main/java/com/ismailmesutmujde/kotlinretrofitcryptomoney/view/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlinretrofitcryptomoney/view/MainActivity.kt
@@ -3,9 +3,19 @@ package com.ismailmesutmujde.kotlinretrofitcryptomoney.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ismailmesutmujde.kotlinretrofitcryptomoney.R
+import com.ismailmesutmujde.kotlinretrofitcryptomoney.model.CryptoModel
+import com.ismailmesutmujde.kotlinretrofitcryptomoney.service.CryptoAPI
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val BASE_URL = "https://raw.githubusercontent.com/"
+    private  var cryptoModels : ArrayList<CryptoModel>? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,6 +26,38 @@ class MainActivity : AppCompatActivity() {
         //https://api.nomics.com/v1/
         //prices?key=2187154b76945f2373394aa34f7dc98a
 
+        loadData()
+    }
 
+    private fun loadData() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val service = retrofit.create(CryptoAPI::class.java)
+        val call = service.getData()
+
+        call.enqueue(object: Callback<List<CryptoModel>> {
+            override fun onResponse(
+                call: Call<List<CryptoModel>>,
+                response: Response<List<CryptoModel>>
+            ) {
+                if(response.isSuccessful) {
+                    response.body()?.let {
+                        cryptoModels = ArrayList(it)
+                        for(cryptoModel : CryptoModel in cryptoModels!!) {
+                            println(cryptoModel.currency)
+                            println(cryptoModel.price)
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<List<CryptoModel>>, t: Throwable) {
+                t.printStackTrace()
+            }
+
+        })
     }
 }
